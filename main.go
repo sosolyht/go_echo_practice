a60087bd3a386674c59807c35932c078660ccde2
sosolyht <sosolyht@gmail.com>
2022-07-08 14:10:53 +0900

    echo 튜토리얼 진행 Query Parameters

--- main.go
+++ main.go
@@ -11,11 +11,20 @@ func getUser(c echo.Context) error {
 	return c.String(http.StatusOK, id)
 }
 
+func getFamily(c echo.Context) error {
+	name := c.QueryParam("name")
+	member := c.QueryParam("member")
+	return c.String(http.StatusOK, "name:"+name+",member:"+member)
+}
+
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World")
 	})
 	e.GET("/users/:id", getUser)
+
+	// /family?name=choi&member=family
+	e.GET("/family", getFamily)
 	e.Logger.Fatal(e.Start(":8000"))
 }
