e63acbea040660a15d9a33fb77f26108e5a5d1db
sosolyht <sosolyht@gmail.com>
2022-07-08 14:18:07 +0900

    echo 튜토리얼 진행 post
    json 데이터를 정상적으로 받아왔을시 201 반환
    
    참고:
    https://user-images.githubusercontent.com/2377807/177922031-5404738a-bbe7-4cc7-ae39-f15acf88bc9e.png

--- main.go
+++ main.go
@@ -6,6 +6,11 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+type User struct {
+	Name  string `json:"name" query:"name"`
+	Email string `json:"email" query:"email"`
+}
+
 func getUser(c echo.Context) error {
 	id := c.Param("id")
 	return c.String(http.StatusOK, id)
@@ -21,6 +26,13 @@ func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World")
+	})
+	e.POST("/users", func(c echo.Context) error {
+		u := new(User)
+		if err := c.Bind(u); err != nil {
+			return err
+		}
+		return c.JSON(http.StatusCreated, u)
 	})
 	e.GET("/users/:id", getUser)
 
