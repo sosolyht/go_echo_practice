0f59cc8e46557b49dc70879fd8529e9e10dba632
sosolyht <sosolyht@gmail.com>
2022-07-08 14:04:33 +0900

    echo 튜토리얼 진행 Path Parameters

--- main.go
+++ main.go
@@ -6,10 +6,16 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+func getUser(c echo.Context) error {
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
+}
+
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World")
 	})
+	e.GET("/users/:id", getUser)
 	e.Logger.Fatal(e.Start(":8000"))
 }
