7631758bc8060f297db2acd175f43289f3ce786e
sosolyht <sosolyht@gmail.com>
2022-07-15 18:51:39 +0900

    media 엔티티 수정

--- model/media.go
+++ model/media.go
@@ -4,10 +4,11 @@ import "time"
 
 type Media struct {
 	Id         string    `gorm:"primaryKey;autoIncrement:true"`
-	Size       string    `gorm:"size:50;not null"`
-	Codec      string    `gorm:"size:20;not null"`
+	Title      string    `gorm:"size:100;not null"` // filename 은 aws.com/ <= 슬래쉬 뒤의 파일명으로 반환 하면 될듯?
+	Size       string    `gorm:"size:50;not null"`  // 224749240 <= 이런 형식인데..어뜨케 mb 나 gb 로 변환?..
+	Codec      string    `gorm:"size:20;not null"`  // codec_long_name 의 첫번째를 split 해서 [0] 번째꺼
 	Duration   string    `gorm:"size:30;not null"`
-	Resolution string    `gorm:"size:15;not null"`
+	Resolution string    `gorm:"size:15;not null"` // resolution 은 width 와 height 붙여서 저장 ex) 1920 + x + 1080
 	CreatedAt  time.Time `gorm:"type:datetime(6);not null"`
 	UpdatedAt  time.Time `gorm:"type:datetime(6);not null"`
 }
