31ea46b2f76bde47521713a4e6bb5a0e83f367d8
sosolyht <sosolyht@gmail.com>
2022-07-18 15:44:02 +0900

    왜 이건 안나오는거지?

--- handler/generatemetadata.go
+++ handler/generatemetadata.go
@@ -5,16 +5,19 @@ import (
 	"os/exec"
 )
 
-func FFprobe(url string) map[string]interface{} {
-	var result map[string]interface{}
+type result struct {
+	Streams string `json:"streams"`
+}
 
+func FFprobe(url string) *result {
+	var a result
 	cmd := exec.Command("ffprobe", "-show_streams", "-show_format", "-print_format", "json", url)
 	cmdOutput, err := cmd.Output()
 	if err != nil {
 		log.Error(err)
 	}
 	cmdStr := string(cmdOutput)
-	json.Unmarshal([]byte(cmdStr), &result)
+	json.Unmarshal([]byte(cmdStr), &a)
 
-	return result
+	return &a
 }
