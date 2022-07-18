bd9760cbe810e4fa69bbcdc9389a05e75f765422
sosolyht <sosolyht@gmail.com>
2022-07-18 15:10:54 +0900

    cleanup code

--- handler/generatemetadata.go
+++ handler/generatemetadata.go
@@ -6,17 +6,16 @@ import (
 	"os/exec"
 )
 
-type good struct{}
-
 func FFprobe(url string) map[string]interface{} {
-	result := exec.Command("ffprobe", "-show_streams", "-show_format", "-print_format", "json", url)
-	result1, err := result.Output()
+	var result map[string]interface{}
+
+	cmd := exec.Command("ffprobe", "-show_streams", "-show_format", "-print_format", "json", url)
+	cmdOutput, err := cmd.Output()
 	if err != nil {
 		log.Fatal(err)
 	}
-	result2 := string(result1)
-	var a map[string]interface{}
-	json.Unmarshal([]byte(result2), &a)
+	cmdStr := string(cmdOutput)
+	json.Unmarshal([]byte(cmdStr), &result)
 
-	return a
+	return result
 }
